8f00c4c6ae156005e23c6b1d530970d4f24210f7
sanjay chudali <poudelsanjay93@gmail.com>
2026-04-08 21:16:02 +0545

    Update email message frequency

--- core/tasks.py
+++ core/tasks.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 def send_email_task():
     send_mail(
         subject="Scheduled Email",
-        message="This is an automated email sent every 10 seconds.",
+        message="This is an automated email sent every 1 hours.",
         from_email="pooudellsanjay885@gmail.com",
         recipient_list=["powdelsanjay50@gmail.com"],
         fail_silently=False,
